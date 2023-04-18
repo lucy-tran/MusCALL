74b529cd3f05c9c15b0fbb3ee5ef9f85c721e257
Lucy Tran <ltran2@macalester.edu>
2023-04-18 00:09:55 -0500

    Updated test.py to test the Final_20 test set from Imagination

--- scripts/test.py
+++ scripts/test.py
@@ -5,7 +5,7 @@ import csv
 
 from muscall.utils.utils import get_root_dir
 
-output_file_prefix = "Imagination"
+output_file_prefix = "Final"
 
 def test(model_id, test_set_size, trial):
     command = "python3 evaluate.py %s retrieval %s_%s_%s %s" %(model_id, output_file_prefix, test_set_size, trial, test_set_size) 
@@ -15,29 +15,39 @@ experiments_path = os.path.join("..", "save", "experiments")
 experiments = os.listdir(experiments_path)
 experiments.remove('get_best.py')
 experiments.remove('records.tsv')
-experiments.remove('2023-04-03-09_07_00')
+
+num_trials = 1
 
 with open("results.tsv", "w", encoding='utf8', newline='') as record_file:
     tsv_writer = csv.writer(record_file, delimiter='\t', lineterminator='\n')
-    tsv_writer.writerow(["experiment_id", "test_set_size", "best_R@10", "from trial#"])
 
     for exp in experiments:
-        for test_set_size in range(20, 101, 20):
-            best_R_10 = 0.0
-            best_trial = 1
+        best_R10s = {}
+        avg_R10s = {}
+        test_set_size = 20
+        best_R_10 = 0.0
+        total_R_10 = 0.0
+        best_trial = 1
 
-            for trial in range(1, 6):
-                test(exp, test_set_size, trial)
-                output_file_path = os.path.join(experiments_path, exp, "%s_%s_%s.txt" %(output_file_prefix, test_set_size, trial))
-                
-                with open(output_file_path, 'r') as file:
-                    retrieval_metrics = json.loads(file.readline())
+        for trial in range(1, num_trials+1):
+            test(exp, test_set_size, trial)
 
-                    trial_R_10 = retrieval_metrics["R@10"]
-                    print(trial_R_10)
+            output_file_path = os.path.join(experiments_path, exp, "%s_%s_%s.txt" %(output_file_prefix, test_set_size, trial))
+            
+            with open(output_file_path, 'r') as file:
+                retrieval_metrics = json.loads(file.readline())
 
-                    if trial_R_10 > best_R_10:
-                        best_R_10 = trial_R_10
-                        best_trial = trial
+                trial_R_10 = retrieval_metrics["R@2"]
+                total_R_10 += trial_R_10
 
-            tsv_writer.writerow([exp, test_set_size, best_R_10, best_trial])
+                if trial_R_10 > best_R_10:
+                    best_R_10 = trial_R_10
+                    best_trial = trial
+
+        best_R10s[test_set_size] = best_R_10
+        avg_R10s[test_set_size] = total_R_10 / num_trials
+
+        tsv_writer.writerow([exp, *best_R10s.values(), *avg_R10s.values()])
+
+# best_R10s = {20: 1, 40: 2, 60: 3}
+# print(*best_R10s.values())
